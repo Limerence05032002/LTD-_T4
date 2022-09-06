73455f49a56020a05537a68fd6c90f7411877cc8
Trung Anht <frontendstack2002@gmail.com>
2022-09-06 21:13:06 +0700

    Bài 3 tuần 2

---
+++ Bai3/app/src/main/java/com/example/bai3/actived.java
@@ -0,0 +1,14 @@
+package com.example.bai3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class actived extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actived);
+    }
+}
