75ba9ae47c4748a491d9ef1f0f3d9947bcb9a418
NEWUSER <frontendstack2002@gmail.com>
2022-10-05 16:20:13 +0700

    Get Contacts phone

---
+++ Content_Provider/app/src/main/java/com/example/content_provider/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.content_provider;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnGetContactPressed(View v){
+        getPhoneContacts();
+    }
+
+    private void getPhoneContacts(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_CONTACTS}, 0);
+        }
+
+        ContentResolver contentResolver = getContentResolver();
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        Cursor cursor = contentResolver.query(uri, null,null,null,null);
+        Log.i("CONTACT_PROVIDER", "TOTAL # of Contacts ::: " + Integer.toString(cursor.getCount()));
+        if (cursor.getCount() > 0){
+            while (cursor.moveToNext()) {
+                String contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                String contactNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+                Log.i("CONTACT_PROVIDER","Contact Name :::  " + contactName + " Ph #  ::: " + contactNumber);
+
+            }
+        }
+    }
+}
