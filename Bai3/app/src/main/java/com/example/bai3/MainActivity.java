8c6542744db931b49023fa8fe8f6b9d47420550c
NEWUSER <frontendstack2002@gmail.com>
2022-10-12 15:36:41 +0700

    Parse file Json

--- Bai3/app/src/main/java/com/example/bai3/MainActivity.java
+++ Bai3/app/src/main/java/com/example/bai3/MainActivity.java
@@ -2,35 +2,78 @@ package com.example.bai3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    ListView lvMonHoc;
-    ArrayList<String> arrayCourse;
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lvMonHoc = findViewById(R.id.lvMonHoc);
-        arrayCourse = new ArrayList<>();
+        mTextViewResult = findViewById(R.id.text_view_result);
+        Button buttonParse = findViewById(R.id.button_parse);
 
-        arrayCourse.add("1");
-        arrayCourse.add("2");
-        arrayCourse.add("3");
-        arrayCourse.add("4");
-        arrayCourse.add("5");
-        arrayCourse.add("6");
+        mQueue = Volley.newRequestQueue(this);
 
-        ArrayAdapter adapter =new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, arrayCourse);
+        buttonParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                jsonParse();
+            }
+        });
+    }
 
-        lvMonHoc.setAdapter(adapter);
+    private void jsonParse() {
+        String url = "https://api.jsonserve.com/NTIoS8";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("employee");
+
+                    for (int i = 0; i<jsonArray.length(); i++){
+                        JSONObject employees = jsonArray.getJSONObject(i);
+
+                        String firstname= employees.getString("firstname");
+                        int age = employees.getInt("age");
+                        String lastname = employees.getString("lastname");
+
+                        mTextViewResult.append(firstname + "," + String.valueOf(age)+ "," + lastname + "\n\n");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
     }
 }
